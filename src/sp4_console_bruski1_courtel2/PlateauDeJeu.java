48f2d605b88549269b78b9ee7e9afbad01fe1a19
Ilan Bruski <ilanbruski@gmail.com>
2022-11-28 14:29:22 +0100

    TPSuperPuissance4 - 4.3 création de CelluleGrille

--- src/sp4_console_bruski1_courtel2/PlateauDeJeu.java
+++ src/sp4_console_bruski1_courtel2/PlateauDeJeu.java
@@ -9,5 +9,6 @@ package sp4_console_bruski1_courtel2;
  * @author ilanb
  */
 public class PlateauDeJeu {//création d'une nouvelle classe
-    
+    private CelluleDeGrille[][]grille=new CelluleDeGrille[6][7];//il s’agit d’un tableau de 6 par 7 objets de type CelluleDeGrille
+
 }
