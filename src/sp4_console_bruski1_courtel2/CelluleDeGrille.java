67ec5bda3a67b046a71aa9eda690a2d3fff84e3e
jules courtel <jules.courtel@epfedu.fr>
2022-12-04 12:06:05 +0100

    SP4_console_BRUSKI1-_COURTEL2 Finalisation de la  classe CelluleDeGrille avec Trounoir et Integrateurs + commentaires

--- src/sp4_console_bruski1_courtel2/CelluleDeGrille.java
+++ src/sp4_console_bruski1_courtel2/CelluleDeGrille.java
@@ -9,54 +9,117 @@ package sp4_console_bruski1_courtel2;
  * @author jules
  */
 public class CelluleDeGrille { //création de la class CelluleDeGrille
-    private Jeton jetonCourant; //ajout attribut "jetonCourant" associé au Jeton
+    private Jeton jetonCourant ; //ajout attribut "jetonCourant" associé au Jeton
+    private boolean avoirTrouNoir ;
+    private boolean avoirDesintegrateur ;  
+    /*ajout des attributs spécifique au super puissance 4*/
     
-    public CelluleDeGrille() { //initialisation de l'attribut
-        jetonCourant = null;
+    public CelluleDeGrille() { //initialisation des attributs
+        jetonCourant = null ;
+        avoirTrouNoir = false ;
+        avoirDesintegrateur = false ;
     }
     
     public boolean presenceJeton() {/*permet de savoir si la cellule est en
         présence ou non d'un jeton grâce à la méthode boolean*/
         if (jetonCourant == null) {
-            return false;
+            return false ;
         }
         else {
-            return true;
+            return true ; 
         }
     }
     
     public void affecterJeton(Jeton un_jeton) {/*on ajoute le jeton en parametre a la cellule*/    
-        jetonCourant = un_jeton;
+        jetonCourant = un_jeton ;
     }
     
     public String lireCouleurDuJeton() {/*on renvoie la couleur du jeton si il est présent */  
         if (jetonCourant == null) {
-            return "la case est vide";
+            return "la case est vide" ;
         }
         else {
-            return jetonCourant.lireCouleur();  
+            return jetonCourant.lireCouleur() ;  
         }
     }
     
-    public Jeton recupererJeton() {
-        Jeton temp = jetonCourant;
-        jetonCourant = null;
-        return (temp);
+    public void placerTrouNoir() {
+        avoirTrouNoir = true ;
     }
+    
+    public void supprimerTrouNoir() { 
+        avoirTrouNoir = false ;
+    }
+    
+    public boolean presenceTrouNoir() {
+    /*accesseur en lecture pour les trous noires*/
+        if (avoirTrouNoir == true) {
+            return true ;
+        } else {
+            return false ;
+        }
+    }
+    
+    public Jeton recupererJeton() {/*variable temporaire permet de recupérer
+        le jeton au cours du jeu*/
+        Jeton temp = jetonCourant ;
+        jetonCourant = null ;
+        return (temp) ;
+    }
+    
+    public void supprimerJeton() {
+        jetonCourant = null ;
+    }
+    
+    public boolean presenceDesintegrateur() {
+    /*accesseur en lecture pour les intégrrateurs*/
+        if (avoirDesintegrateur == true) {
+            return true ;
+        } else {
+            return false ;
+        }
+    }
+    
+    public void placerDesintegrateur() {
+       avoirDesintegrateur = true ;
+    }
+   
+    public void supprimerDesintegrateur() {
+       avoirDesintegrateur = false ;
+    }
+    
+    public void activerTrouNoir() { /*methode permettant activer le trou noir et 
+       et donc de supprimer le jeton de la case */
+        this.supprimerJeton() ;
+        this.supprimerTrouNoir() ;
+    }
+    
     @Override//ecrase
 public String toString (){//creation de la methode toString
     String clr=lireCouleurDuJeton();
     String chaine_a_retourner ;//initialisation d'une variable
-    chaine_a_retourner="erreur";
-    if (jetonCourant==null){
-        chaine_a_retourner=".";
+    chaine_a_retourner="erreur" ;
+    
+    if (jetonCourant==null && avoirDesintegrateur == false &&avoirTrouNoir == false){
+        chaine_a_retourner="." ;
     }
+    
     if(clr=="rouge"){
-        chaine_a_retourner="R";
+        chaine_a_retourner="R" ;
     }
+    
     if(clr=="jaune"){
-        chaine_a_retourner="J";
+        chaine_a_retourner="J" ;
     }
-return chaine_a_retourner;//renvoi de la variable au systee en interne
-}
+    
+    if(avoirTrouNoir == true) {
+        chaine_a_retourner= "@" ;
+    }
+    
+    if(avoirDesintegrateur == true) {
+        chaine_a_retourner= "D" ;
+    }
+    // retourne les caracteres en fonction des conditions de chaques cases
+    return chaine_a_retourner ;//renvoi de la variable au systee en interne
+    }
 }
