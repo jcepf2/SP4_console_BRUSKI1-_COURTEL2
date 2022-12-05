8ed7825b84d99e978d38ad8402d40bdd9ea0ea90
jules courtel <jules.courtel@epfedu.fr>
2022-12-05 09:21:44 +0100

    SP4_console_BRUSKI1-_COURTEL2

---
+++ src/sp4_console_bruski1_courtel2/CelluleGraphique.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_bruski1_courtel2;
+
+import javax.swing.JButton;
+import sp4_console_bruski1_courtel2.CelluleDeGrille;
+
+/**
+ *
+ * @author jules
+ */
+
+public class CelluleGraphique extends JButton {
+    CelluleDeGrille celluleAssociee;
+    
+}
